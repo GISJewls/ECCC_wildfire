a35c845c75db808966738b6a23809c123ab25dfc
Julie Duval <41807185+GISJewls@users.noreply.github.com>
2026-03-16 09:27:08 -0600

    Improve script flow

--- wildfire_polygons_to_raster.py
+++ wildfire_polygons_to_raster.py
@@ -1,8 +1,8 @@
 #----------------------------------------------------------------------------------
 #            Wildfire Metrics: Area Burned and Annual Wildfire Rasters
 #----------------------------------------------------------------------------------
-# SCRIPT NAME:  wildfire_metrics_polygons.py
-#               v.2026.0305
+# SCRIPT NAME:  wildfire_polygons_to_raster.py
+#               v.2026.0313
 #
 # PURPOSE:      Calculate wildfire metrics within defined caribou zones.
 #
@@ -10,14 +10,15 @@
 #              --------------------------------------------------------------------
 #               aoi         <R>     Area of interest (herd boundaries)
 #               aoi_fld     <R>     Unique field from aoi
-#               wildfires   <R>     Annual wildfire data, polygons
-#               wfYear      <R>     Wildfire year, integer
+#               wildfireList <R>     Annual wildfire data, polygons (multiple)
+#               wf_year_fld <R>     Wildfire year, field
+#               wf_id_fld   <R>     Wildfire identifier, field
 #               wfPrefix    <R>     Prefix used for outputs, string
 #               outWksp     <R>     Output workspace
 #               snapRst     <O>     Snap raster
 #               outCellSize <R>     Output raster cell size, default = 30m
 #
-# OUTPUTS:      summary table and integer raster
+# OUTPUTS:      summary tables and integer attributed raster
 #
 # AUTHOR:       Julie Duval, fRI Research
 #
@@ -26,6 +27,7 @@
 # EDITORS:      Julie Duval, fRI Research
 #
 # LAST UPDATES: March 3, 2026
+#               March 13, 2026  - debugging and improving code
 #
 # NOTES:        tested with ArcGIS PRO 3.6.2
 #               requires Advanced licensing with Spatial Analyst extension
@@ -38,7 +40,6 @@ import traceback
 import arcpy
 from arcpy import env
 from os.path import join as fp
-from CheckLicenses import CheckArcInfo, CheckSpatialExt
 from CheckLicenses import CheckArcInfo, CheckSpatialExt
 from functions import isNameOk, isPathOk, display, error, warning
 
@@ -56,7 +57,7 @@ global log
 try:
     fpath = os.path.split(sys.path[0])[0]
     starttime = datetime.datetime.now()
-    logFile = fp(fpath, 'polymetrics_log.txt')
+    logFile = fp(fpath, 'polygons_to_raster_log.txt')
     log = open(logFile, 'a')
     log.write('\n\n' + '=' * 89 + '\nTool Started on: ' +
                 str(starttime.strftime('%d-%b-%Y %I:%M %p')) + '\n')
@@ -66,7 +67,7 @@ except:
 #----------------------------------------------------------------------------------
 # Fire Metrics
 #==================================================================================
-def FireMetrics(args):
+def WildfirePolygonsToRaster(args):
     try:
         # Only proceed if the required ArcGIS licenses are available
         if CheckArcInfo() == "yes" and CheckSpatialExt() == "yes":
@@ -79,7 +80,7 @@ def FireMetrics(args):
                 'Please verify spatial reference systems (SRS). \n'
                 'Outputs will default to the SRS of the Wildfires layer. \n'
                 'If the spatial references do not match, please reproject \n'
-                'the input layers and rerun the tool. \n\n'
+                'the input layers and rerun the tool. '
                 '\n' + "*" * 60 + '\n', log
             )
 
@@ -91,11 +92,13 @@ def FireMetrics(args):
             # inputs
             aoi = arcpy.GetParameterAsText(0)
             aoi_fld = arcpy.GetParameterAsText(1)
-            wildfire_year = arcpy.GetParameterAsText(2)
-            wfPrefix = arcpy.GetParameterAsText(3)
-            outWksp = arcpy.GetParameterAsText(4)
-            snapRst = arcpy.GetParameterAsText(5)
-            outCellSize = int(arcpy.GetParameterAsText(6))
+            wildfireList = arcpy.GetParameterAsText(2).split(';')
+            wf_year_fld = arcpy.GetParameterAsText(3)
+            wf_id_fld = arcpy.GetParameterAsText(4)
+            wfPrefix = arcpy.GetParameterAsText(5)
+            outWksp = arcpy.GetParameterAsText(6)
+            snapRst = arcpy.GetParameterAsText(7)
+            csvWksp = arcpy.GetParameterAsText(8)
 
             #======================================================================
             # Test naming conventions
@@ -111,109 +114,261 @@ def FireMetrics(args):
             #----------------------------------------------------------------------
             arcpy.env.workspace = outWksp
             arcpy.env.extent = aoi
-            if snapRst != '':
-                arcpy.env.snapRaster = snapRst
             arcpy.env.mask = aoi
-            arcpy.env.cellSize = outCellSize
-
-            #==================================================================
-            # ANALYSIS
-            #==================================================================
-            display(' ... Intersecting aoi polygons with wildfires', log)
+            arcpy.env.snapRaster = snapRst
+            arcpy.env.cellSize = snapRst
 
             #======================================================================
-            # Get wildfire datasets and years
+            # ANALYSIS
+            #======================================================================
+            # create list to keep track of years with no wildfires
+            noWildfires = []
+
+            #======================================================================
+            # Loop through wildfire datasets
             #----------------------------------------------------------------------
-            value_table = arcpy.ValueTable(2)
-            value_table.loadFromString(wildfire_year)
+            for i in range(0, len(wildfireList)):
 
-            for i in range(0, value_table.rowCount):
-                row = value_table.getRow(i)
+                wildfires = wildfireList[i]
 
-                wildfires = value_table.getValue(i, 0)
-                wfYear = value_table.getTrueValue(i, 1)
+                # get year of wildfire
+                with arcpy.da.SearchCursor(wildfires, [wf_year_fld]) as cursor:
+                    for row in cursor:
+                        wfYear = int(float(row[0]))
+                        break
+
+                # get wildfire id field type
+                fields = arcpy.ListFields(wildfires)
+                for field in fields:
+                    if field.name == wf_id_fld:
+                        fType = field.type
+                        if fType == 'TEXT':
+                            fLength = field.length
+
+                display('\n ... Processing for wildfire year ' + str(wfYear), log)
 
                 # -----------------------------------------------------------------
                 # Step 1: Pairwise Intersect aoi with wildfire polgyons
                 # -----------------------------------------------------------------
-                display(' ... Intersecting aoi polygons with wildfires', log)
+                display('     - Intersecting aoi polygons with wildfires', log)
 
                 inFeatures = [aoi, wildfires]
-                outFC = r'memory\FC_pi'
-                arcpy.analysis.PairwiseIntersect(inFeatures, outFC, 'ALL')
+                tempFC = r'memory\FC_pi'
+                arcpy.analysis.PairwiseIntersect(inFeatures, tempFC, 'ALL')
 
-                # -----------------------------------------------------------------
-                # Step 2: Summarize polygons amd calculate sum of Areas (km2)
-                # -----------------------------------------------------------------
-                outTable = wfPrefix + '_' + str(wfYear) + '_sumArea'
-                display(' ... Summarizing polygon areas (km2) to table: ' +
-                        outTable, log)
+                # check if any results exist
+                arcpy.management.MakeFeatureLayer(
+                    in_features = tempFC,
+                    out_layer = 'FC_lyr'
+                )
+                result = arcpy.management.GetCount('FC_lyr')
+                count = int(result[0])
 
-                # Add and calcualte 'AREA_KM' in square kilometers
-                try:
+                if count != 0:
+                    # -------------------------------------------------------------
+                    # Step 2: Add and calculate new fields
+                    # -------------------------------------------------------------
+                    statTable = wfPrefix + '_' + str(wfYear) + '_sumArea'
+                    display('     - Adding new fields to polygon layer', log)
+
+                    # Add and calculate 'AREA_KM2' in square kilometers
                     arcpy.management.AddField(
-                        in_table = r'memory\FC_pi',
-                        field_name = 'WF_AREA_KM',
+                        in_table = tempFC,
+                        field_name = 'POLY_AREA_KM2',
                         field_type = 'DOUBLE',
                     )
                     arcpy.management.CalculateGeometryAttributes(
-                        in_features = r'memory\FC_pi',
-                        geometry_property = [['WF_AREA_KM', 'AREA']],
+                        in_features = tempFC,
+                        geometry_property = [['POLY_AREA_KM2', 'AREA']],
                         area_unit = 'SQUARE_KILOMETERS'
                     )
 
-                except:
-                    tbinfo = traceback.format_tb(sys.exc_info()[2])[0]
-                    error('\nPYTHON ERRORS in CalculateGeometryAttributes():'
-                          '\nTraceback Info:    ' + tbinfo +
-                          '\nError Type:    ' + str(sys.exc_info()[0]) +
-                          '\nError Info:    ' + str(sys.exc_info()[1]), log)
+                    # Add and calculale 'HERD_YEAR_FIREID'
+                    fld = 'HERD_YEAR_FIREID'
+                    if fType == 'TEXT':
+                        fStr = '!{}! + "_" + str(int(!{}!)) + "_" + str(!{}!)'
+                    else:
+                        fStr = '!{}! + "_" + str(int(!{}!)) + "_" + str(int(!{}!))'
 
-                arcpy.analysis.Statistics(
-                    in_table = r'memory\FC_pi',
-                    out_table = outTable,
-                    statistics_fields = [['WF_AREA_KM', 'SUM']],
-                    case_field = aoi_fld,
-                    concatenation_separator = ''
-                )
+                    expr = fStr.format(aoi_fld, wf_year_fld, wf_id_fld)
 
-                # ---------------------------------------------------------------------
-                # Step 3: Create raster from polygons
-                # ---------------------------------------------------------------------
-                display(' ... Converting polygons to raster ' +
-                        wfPrefix + '_' + str(wfYear), log)
-                outRst = fp(outWksp, wfPrefix + '_' + str(wfYear))
+                    arcpy.management.AddField(
+                        in_table = tempFC,
+                        field_name = fld,
+                        field_type = 'TEXT',
+                        field_length = 50
+                    )
 
-                arcpy.conversion.PolygonToRaster(
-                    in_features = outFC,
-                    value_field = 'YEAR',
-                    out_rasterdataset = outRst,
-                    cell_assignment = 'MAXIMUM_COMBINED_AREA'
-                )
+                    fld_desc = [[fld, 'TEXT', '', 50]]
 
-                # ---------------------------------------------------------------------
-                # Step 6: Memory clean up
-                # ---------------------------------------------------------------------
-                display(' ... Cleaning up temporary data from memory', log)
-                memList = [r'memory\rst1', r'memory\FC_pi']
+                    arcpy.management.AddFields(
+                        in_table = tempFC,
+                        field_description = fld_desc
+                    )
+
+                    arcpy.management.CalculateField(
+                        in_table = tempFC,
+                        field = fld,
+                        expression = str(expr)
+                    )
+
+                    # -------------------------------------------------------------
+                    # Step 3: Summarize polygon areas by herd, year, and fireID
+                    # -------------------------------------------------------------
+                    display('     - Summarizing polygon areas', log)
+                    arcpy.analysis.Statistics(
+                        in_table = tempFC,
+                        out_table = statTable,
+                        statistics_fields = [['POLY_AREA_KM2', 'SUM']],
+                        case_field = ['HERD_YEAR_FIREID'],
+                    )
+
+                    # -------------------------------------------------------------
+                    # Step 4: Convert polygons to raster
+                    # -------------------------------------------------------------
+                    display('     - Converting polygons to raster: ' +
+                            wfPrefix + '_' + str(wfYear), log)
+                    outRst = fp(outWksp, wfPrefix + '_' + str(wfYear))
+
+                    arcpy.conversion.PolygonToRaster(
+                        in_features = tempFC,
+                        value_field = fld,
+                        out_rasterdataset = outRst,
+                        cell_assignment = 'MAXIMUM_COMBINED_AREA'
+                    )
+
+                    # -------------------------------------------------------------
+                    # Step 5: Add new fields to raster attribute table
+                    # -------------------------------------------------------------
+                    display('     - Adding new fields to raster attribute table',
+                            log)
+
+                    # Create a copy of the raster table as a workaround.
+                    # Working directly from the raster table created unknown and
+                    # (at the time) unsolvable errors
+                    rstTable = fp(outWksp, wfPrefix + '_' + str(wfYear) + '_tbl')
+                    arcpy.conversion.ExportTable(
+                        in_table = outRst,
+                        out_table = rstTable
+                    )
+
+                    if fType == 'TEXT':
+                        addFL = 'TEXT, "", ' + str(fLength)
+                    else:
+                        addFL = fType
+
+                    fld_desc = [['HERD', 'TEXT', '', 20],
+                                ['PROV', 'TEXT', '', 20],
+                                ['YEAR', 'SHORT'],
+                                ['FIREID', addFL],
+                                ['POLY_AREA_KM2', 'DOUBLE'],
+                                ['RAST_AREA_KM2', 'DOUBLE']]
+
+                    arcpy.management.AddFields(
+                        in_table = rstTable,
+                        field_description = fld_desc
+                    )
+
+                    # -------------------------------------------------------------
+                    # Step 6: Calculate attributes
+                    # -------------------------------------------------------------
+                    display('     - Calculating attributes', log)
+
+                    # create raster table view for join operation
+                    arcpy.management.MakeTableView(
+                        in_table = rstTable,
+                        out_view = 'rstTbl_View'
+                    )
+
+                    # join raster table and polygon tables
+                    join_table = arcpy.management.AddJoin(
+                        in_layer_or_view = 'rstTbl_View',
+                        in_field = fld,
+                        join_table = statTable,
+                        join_field = fld
+                    )
+
+                    # grab field name prefixes from joined tables
+                    flds = arcpy.ListFields(join_table)
+                    rtbl_prefix = flds[0].name.split('.')[0]
+                    jtbl_prefix = flds[len(flds)-1].name.split('.')[0]
+
+                    # field names to calculate
+                    fldNames = ['HERD', 'PROV', 'YEAR', 'FIREID',
+                                'POLY_AREA_KM2', 'RAST_AREA_KM2']
+
+                    # define expressions for calculation
+                    exprList = [
+                        '!{}!.split("_")[0]'.format(jtbl_prefix + '.' + fld),
+                        '!{}!.split("_")[1]'.format(jtbl_prefix + '.' + fld),
+                        'int(!{}!.split("_")[2])'.format(jtbl_prefix + '.' + fld),
+                        'str(!{}!.split("_")[3])'.format(jtbl_prefix + '.' + fld),
+                        '!{}!'.format(jtbl_prefix + '.SUM_POLY_AREA_KM2'),
+                        '!{}! * 0.0009'.format(str(rtbl_prefix + '.Count'))
+                    ]
+
+                    # loop to calculate fields
+                    for i in range(0, len(fldNames)):
+                        arcpy.management.CalculateField(
+                            in_table = 'rstTbl_View',
+                            field = fldNames[i],
+                            expression = exprList[i]
+                        )
+
+                    # remove join
+                    arcpy.management.RemoveJoin('rstTbl_View')
+
+                    # -------------------------------------------------------------
+                    # Step 7: Export final table to csv
+                    # -------------------------------------------------------------
+                    display('     - Exporting raster attributes to csv', log)
+
+                    tblExport = fp(csvWksp, wfPrefix + '_' + str(wfYear) + '.csv')
+                    arcpy.conversion.ExportTable(
+                        in_table = rstTable,
+                        out_table = tblExport
+                    )
+
+                    # clear workspace cache
+                    arcpy.management.ClearWorkspaceCache()
+
+                # no records available
+                else:
+                    display('     !!! No wildfires found for ' + str(wfYear), log)
+                    noWildfires.append(wfYear)
+
+                # -----------------------------------------------------------------
+                # Step 8: Memory clean up
+                # -----------------------------------------------------------------
+                memList = [tempFC, rstTable, statTable]
                 for mem in memList:
-                    if arcpy.Exists(mem): arcpy.management.Delete(mem)
+                    if arcpy.Exists(mem):
+                        arcpy.management.Delete(mem)
 
+            # display list of years with no wildfires (if any)
+            numYears = len(noWildfires)
+            if numYears > 0:
+                display('\nThe following {} years had no wildfires occur within '
+                        'the area of interest'.format(str(numYears)), log)
+                for yr in noWildfires:
+                    display(str(yr), log)
+
+            # close log file
             log.close()
 
     except:
         tbinfo = traceback.format_tb(sys.exc_info()[2])[0]
-        error('\nPYTHON ERRORS in main():'
+        error('\nPYTHON ERRORS in WildfirePolygonsToRaster():'
               '\nTraceback Info:    ' + tbinfo +
               '\nError Type:    ' + str(sys.exc_info()[0]) +
               '\nError Info:    ' + str(sys.exc_info()[1]), log)
         log.close()
 
         # release data in memory
-        memList = [r'memory\FC_pi', r'memory\rst1']
+        memList = [tempFC, rstTable, statTable]
         for mem in memList:
             if arcpy.Exists(mem): arcpy.management.Delete(mem)
-
+        arcpy.management.ClearWorkspaceCache()
 
 if __name__ == '__main__':
-    FireMetrics(sys.argv)
+    WildfirePolygonsToRaster(sys.argv)
